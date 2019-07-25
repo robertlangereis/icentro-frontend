98139a604bebd72921992d44cb9a534585837d79
Vivian da Graca <vivian.da.graca@gmail.com>
2019-07-25 15:47:49 +0200

    kaart verschuift naar locatie

--- my-app/src/component/Kaart/Kaart.js
+++ my-app/src/component/Kaart/Kaart.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react'
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper, Listing, findPlaceFromQuery, fetchPlaces } from 'google-maps-react'
 
 
 class Kaart extends Component {
+
+    // constructor() {
+    //     super()
+    //     this.markers = []
+    // }
+
+    fetchPlaces(mapProps, map) {
+
+
+
+        const { google } = mapProps;
+        const service = new google.maps.places.PlacesService(map);
+        console.log('hoi')
+        var request = {
+            query: 'IJswinkel',
+            fields: ['name', 'geometry'],
+        };
+
+        service.findPlaceFromQuery(request, (results, status) => {
+            const markers = []
+
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                console.log('Got results', results);
+                for (var i = 0; i < results.length; i++) {
+                    markers.push(
+                        <Marker
+                            name={results[i].name}
+                            position={results[i].geometry.location} />
+                    )
+                }
+
+                map.setCenter(results[0].geometry.location);
+            }
+        });
+    }
+
+
+
     render() {
         return (
             <Map
+                onReady={this.fetchPlaces}
                 style={{ width: '50%', height: '50%' }}
                 initialCenter={{
-                    lat: 52.379189,
-                    lng: 4.899431
+                    lat: 51.9224165,
+                    lng: 4.4310972
                 }}
                 google={this.props.google} zoom={14}>
-                <Marker onClick={this.onMarkerClick}
+                {this.markers}
+                <Marker
+                    onClick={this.onMarkerClick}
                     name={'Current location'} />
+                {/* <Listing places="this.state.places" */}
             </Map>
         );
     }
