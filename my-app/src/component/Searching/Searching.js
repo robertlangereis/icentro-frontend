cc641516e6997d99250f0de7ba51e4675a8a9b69
Floris Boersma <floris.boersma@incentro.com>
2019-07-25 12:55:32 +0200

    Added content to searching.js

--- my-app/src/component/Searching/Searching.js
+++ my-app/src/component/Searching/Searching.js
@@ -1,1 +1,14 @@
-// Hier komt iets
+import React, { Component } from 'react'
+
+export class Searching extends Component {
+    render() {
+        return (
+            <div>
+                <h1>We zijn voor je aan het zoeken..</h1>
+                <p>Smelt je al?</p>
+            </div>
+        )
+    }
+}
+
+export default Searching
