faccca3eae2e78757ba2916f96746f426de90c7e
Floris Boersma <floris.boersma@incentro.com>
2019-07-25 14:41:38 +0200

    searching ice component

--- my-app/src/Routing.js
+++ my-app/src/Routing.js
@@ -3,14 +3,14 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import LandingPageContainer from "./component/LandingPage/LandingPageContainer";
 import TooColdDefault from "./component/TooColdDefault";
-// import loaderPage from "./Components/loaderPage";
+import SearchingContainer from "./component/Searching/SearchingContainer";
 // import iceCreamResults from "./Components/iceCreamResults";
 
 const Router = () => (
     <Switch>
-        <Route exact path="/" component={LandingPageContainer} />
-        <Route path="/too-cold" exact component={TooColdDefault} />
-        {/* <Route path="/search" exact component={loaderPage} /> */}
+        {/* <Route exact path="/" component={LandingPageContainer} /> */}
+        {/* <Route path="/too-cold" exact component={TooColdDefault} /> */}
+        <Route exact path="/" component={SearchingContainer} />
         {/* <Route path="/found-icecream" exact component={iceCreamResults} /> */}
         <Route exact path="/" render={() => <Redirect to="/" />} />
     </Switch>
---
+++ my-app/src/component/Searching/SearchingContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Searching from './Searching'
+
+export default class SearchingContainer extends Component {
+    render() {
+        return (
+            <div>
+                <Searching />
+            </div>
+        )
+    }
+}
