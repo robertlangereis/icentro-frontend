99b91f76214e7485582b562a804a140ebbe2b7a7
robert <robert.langereis@gmail.com>
2019-07-25 13:09:39 +0200

    werkt met temperature component:

---
+++ my-app/src/component/Temperature.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./TooCold.css";
+
+class Temperature extends Component {
+
+    
+    render() {
+        return (
+                <div className='temperature'>
+                    <h2>20°C</h2>
+                </div>
+        )
+    }
+}
+
+export default Temperature
--- my-app/src/component/TooColdDefault.js
+++ my-app/src/component/TooColdDefault.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import "./TooCold.css";
+import Temperature from "./Temperature.js";
+import noIcecreamGif from"./no-icecream.gif";
+import tooColdGif from"./too-cold.gif";
 
 class TooColdDefault extends Component {
 
+    
     render() {
         return (
-            <div className='title'>
-                <h1>ICEntro <span>🍦</span></h1>
+            <div className="body">
+                <div className='title'>
+                    <h1>ICEntro <span aria-label="iceCream-Emoji" role="img">🍦</span></h1>
+                    <h2>No man, it's below <Temperature/> No time for IceCream!</h2>
+                    <img className="gif" src={tooColdGif} alt="...it's too cold..." />
+                    <img className="gif" src={noIcecreamGif} alt="no ice cream today..." />
+                </div>
             </div>
         )
     }
